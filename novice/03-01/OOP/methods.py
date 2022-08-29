f93881f3b2af9b3cfb0b11366c176ca3fb76aa36
RosyRizabila <rosyrizabila>
2022-08-29 09:51:17 +0700

    mine

---
+++ novice/03-01/OOP/methods.py
@@ -0,0 +1,15 @@
+# class Person:
+#     def say_hi(self):
+#         print('Hello, how are you?')
+
+# p = Person()
+# p.say_hi()
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print('Hello, my name is', self.name)
+p = Person('Rosy')
+p.say_hi()
